7ebbfd08c93f96bd8c2e18de7fbdeb7735f5d0e9
ViniBelo <vinialvesbelo@outlook.com>
2023-10-11 15:04:16 -0300

    Getting user by ID

--- src/main/java/com/vinibelo/workshopmongo/services/UserService.java
+++ src/main/java/com/vinibelo/workshopmongo/services/UserService.java
@@ -2,11 +2,13 @@ package com.vinibelo.workshopmongo.services;
 
 import com.vinibelo.workshopmongo.domain.entities.User;
 import com.vinibelo.workshopmongo.repositories.UserRepository;
+import com.vinibelo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
     @GetMapping
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    @GetMapping
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
